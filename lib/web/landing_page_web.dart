5636b3243d7fc17eb7261c61c5fa0839f7ff7d6f
Chathuranga97lj <chathuit97@gmail.com>
2023-02-22 14:12:58 +0530

    set second page

--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -61,14 +61,20 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                         EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                     child: SansBold("Hello I'm", 15),
                   ),
-                  SizedBox(height: 15.0,),
+                  SizedBox(
+                    height: 15.0,
+                  ),
                   SansBold("Chathuranga Lakmal", 55.0),
                   Sans("Fullstack Developer", 30.0),
-                  SizedBox(height: 15.0,),
+                  SizedBox(
+                    height: 15.0,
+                  ),
                   Row(
                     children: [
                       Icon(Icons.email),
-                      SizedBox(width: 20,),
+                      SizedBox(
+                        width: 20,
+                      ),
                       Sans("chathuit97@gmail.com", 15.0),
                     ],
                   ),
@@ -76,7 +82,9 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                   Row(
                     children: [
                       Icon(Icons.call),
-                      SizedBox(width: 20,),
+                      SizedBox(
+                        width: 20,
+                      ),
                       Sans("+94 76 8711 780", 15.0),
                     ],
                   ),
@@ -84,7 +92,9 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                   Row(
                     children: [
                       Icon(Icons.location_pin),
-                      SizedBox(width: 20,),
+                      SizedBox(
+                        width: 20,
+                      ),
                       Sans("Palipana Mawatha, Wadupola, Ibbagamuwa", 15.0),
                     ],
                   ),
@@ -105,6 +115,134 @@ class _LandingPageWebState extends State<LandingPageWeb> {
             ],
           ),
         ),
+        // Second Page
+        Container(
+          height: heightDevice / 1.5,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("assets/web.jpg", height: heightDevice/1.7,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SansBold("About me", 40),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Sans(
+                      "Hello! I'm Chathuranga Lakmal I specialize in Mobile and Web Development",
+                      15),
+                  Sans(
+                      "I am a hardworking and ambitious individual with a great passion for Information Technology ",
+                      15),
+                  Sans(
+                      "I am currently working at Efito Solutions as a trainee Software Engineer",
+                      15),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    // Skills
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.tealAccent,
+                            style: BorderStyle.solid,
+                            width: 2.0
+                          ),
+                          borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("Flutter", 15.0),
+                      ),
+                      SizedBox(width: 7.0,),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.tealAccent,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                            ),
+                            borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("React", 15.0),
+                      ),
+                      SizedBox(width: 7.0,),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.tealAccent,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                            ),
+                            borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("React-Native", 15.0),
+                      ),
+                      SizedBox(width: 7.0,),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.tealAccent,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                            ),
+                            borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("MongoDB", 15.0),
+                      ),
+                      SizedBox(width: 7.0,),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.tealAccent,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                            ),
+                            borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("MySQL", 15.0),
+                      ),
+                      SizedBox(width: 7.0,),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.tealAccent,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                            ),
+                            borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("Python", 15.0),
+                      ),
+                      SizedBox(width: 7.0,),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.tealAccent,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                            ),
+                            borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        padding: EdgeInsets.all(7.0),
+                        child: Sans("Java", 15.0),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
       ]),
     );
   }
