fe3e4fdf353013b6ff5e3dad0f08eb0c579b76eb
Chathuranga97lj <chathuit97@gmail.com>
2023-02-22 11:07:42 +0530

    set mobile web selection logic

---
+++ lib/web/landing_page_web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class LandingPageWeb extends StatefulWidget {
+  const LandingPageWeb({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPageWeb> createState() => _LandingPageWebState();
+}
+
+class _LandingPageWebState extends State<LandingPageWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
