95bd90d0960875a158755d17b93f35af1f122a9e
Chathuranga97lj <chathuit97@gmail.com>
2023-02-23 10:01:04 +0530

    set mobile drawer

--- lib/components.dart
+++ lib/components.dart
@@ -52,6 +52,38 @@ class _TabsWebState extends State<TabsWeb> {
   }
 }
 
+class TabsMobile extends StatefulWidget {
+  final text;
+  final route;
+  const TabsMobile({Key? key, @required this.text, @required this.route})
+      : super(key: key);
+
+  @override
+  State<TabsMobile> createState() => _TabsMobileState();
+}
+
+class _TabsMobileState extends State<TabsMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+        elevation: 20.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        height: 50.0,
+        minWidth: 200.0,
+        color: Colors.black,
+        child: Text(
+          widget.text,
+          style: GoogleFonts.openSans(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {});
+  }
+}
+
 class SansBold extends StatelessWidget {
   final text;
   final size;
@@ -174,7 +206,8 @@ class _AnimationCardWebState extends State<AnimationCardWeb>
 
   @override
   Widget build(BuildContext context) {
-    return SlideTransition(position: _animation,
+    return SlideTransition(
+      position: _animation,
       child: Card(
         elevation: 30.0,
         shape: RoundedRectangleBorder(
@@ -191,7 +224,7 @@ class _AnimationCardWebState extends State<AnimationCardWeb>
                 widget.imagePath,
                 height: 200.0,
                 width: 200.0,
-                fit: widget.fit == null ? null:widget.fit,
+                fit: widget.fit == null ? null : widget.fit,
               ),
               SizedBox(height: 10.0),
               SansBold(widget.text, 15.0)
--- lib/mobile/landing_page_mobile.dart
+++ lib/mobile/landing_page_mobile.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio/components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LandingPageMobile extends StatefulWidget {
   const LandingPageMobile({Key? key}) : super(key: key);
@@ -8,8 +10,60 @@ class LandingPageMobile extends StatefulWidget {
 }
 
 class _LandingPageMobileState extends State<LandingPageMobile> {
+  urlLauncher(String imgPath, String url) {
+    return IconButton(
+      icon: Image.asset(imgPath, width: 35.0),
+      onPressed: () async {
+        await launchUrl(Uri.parse(url));
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var widthDevice = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(size: 35.0, color: Colors.black),
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 2.0, color: Colors.black)),
+                child: Image.asset("assets/Chathu_circle.png"),
+              ),
+            ),
+            TabsMobile(text: "Home", route: "/"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Works", route: "/works"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Blog", route: "/blog"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "About", route: "/about"),
+            SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                urlLauncher("assets/linkedin.png",
+                    "https://www.linkedin.com/in/chathuranga97/"),
+                urlLauncher(
+                    "assets/twitter.png", "https://twitter.com/chathu97lj"),
+                urlLauncher(
+                    "assets/github.png", "https://github.com/Chathuranga97lj")
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
